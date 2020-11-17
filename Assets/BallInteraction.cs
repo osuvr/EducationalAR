723fd8b4b1ea6b03e45e900b0a25085d6f25ce13
Arash-sh <shahbaar@oregonstate.edu>
2020-11-16 20:27:20 -0800

    Initial commit of the educational app for AR

---
+++ Assets/BallInteraction.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallInteraction : MonoBehaviour
+{
+    private Vector3 initLocation;
+    // Start is called before the first frame update
+    void Start()
+    {
+        initLocation = transform.position;
+    }
+
+    // Update is called once per frame
+    //void update()
+    //{
+        
+    //}
+
+    public void StartInteraction()
+    {
+        GetComponent<Rigidbody>().useGravity = true;
+    }
+    public void ResetInteraction()
+    {
+        GetComponent<Rigidbody>().useGravity = false;
+        GetComponent<Rigidbody>().velocity = Vector3.zero;
+        GetComponent<Rigidbody>().angularVelocity = Vector3.zero;
+        transform.position = initLocation;
+    }
+}
