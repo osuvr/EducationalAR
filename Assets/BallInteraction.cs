e773af3676602bef20e9ad388b2c404eac2259e9
Arash-sh <2331510+arash-sh@users.noreply.github.com>
2021-02-24 19:54:41 -0800

    added kinetic energy, added time scale capability

--- Assets/BallInteraction.cs
+++ Assets/BallInteraction.cs
@@ -10,11 +10,17 @@ public class BallInteraction : MonoBehaviour
     GameObject heightValueField;
     [SerializeField]
     GameObject potentialValueField;
-    
+    [SerializeField]
+    GameObject kineticValueField;
+    [SerializeField]
+    GameObject totalValueField;
+    [SerializeField]
+    GameObject timeValueField;
     private const float g = 9.8f;
     private Vector3 initLocation;
     private float initMass;
     private bool simulating = false;
+    private float timeScale= .5f;
 
 
     // Start is called before the first frame update
@@ -22,15 +28,35 @@ public class BallInteraction : MonoBehaviour
     {
         initLocation = transform.position;
         initMass = GetComponent<Rigidbody>().mass;
+        Time.timeScale = timeScale;
         RefreshDisplay();
-
-    }
+     }
 
     // Update is called once per frame
     void Update()
     {
-        if(simulating)
+        if (simulating)
             RefreshDisplay();
+
+        if (Input.GetKeyDown("space"))
+        {
+            timeScale = Time.timeScale;
+            Time.timeScale = 0;
+        }
+        if (Input.GetKeyUp("space"))
+            Time.timeScale = timeScale;
+
+        if (Input.GetKeyUp(KeyCode.Plus) || Input.GetKeyUp(KeyCode.KeypadPlus))
+        {
+            Time.timeScale = Mathf.Min(2, Time.timeScale + .1f);
+            RefreshDisplay();
+        }
+
+        if (Input.GetKeyUp(KeyCode.Minus) || Input.GetKeyUp(KeyCode.KeypadMinus))
+        {
+            Time.timeScale = Mathf.Max(.1f, Time.timeScale-.1f);
+            RefreshDisplay();
+       }
     }
 
     public void StartInteraction()
@@ -38,6 +64,7 @@ public class BallInteraction : MonoBehaviour
         GetComponent<Rigidbody>().useGravity = true;
         simulating = true;
     }
+
     public void ResetInteraction()
     {
         GetComponent<Rigidbody>().useGravity = false;
@@ -45,6 +72,7 @@ public class BallInteraction : MonoBehaviour
         GetComponent<Rigidbody>().angularVelocity = Vector3.zero;
         transform.position = initLocation;
         simulating = false;
+        RefreshDisplay();
 
     }
 
@@ -72,13 +100,23 @@ public class BallInteraction : MonoBehaviour
         return GetComponent<Rigidbody>().mass * g * transform.position.y; 
     }
 
+    private float KE() // potential energy
+    {
+        float spd = GetComponent<Rigidbody>().velocity.magnitude;
+        return 0.5f*spd*spd* GetComponent<Rigidbody>().mass;
+    }
 
     private void RefreshDisplay()
     {
+        float ke = KE();
+        float pe = PE();
 
+        timeValueField.GetComponent<UnityEngine.UI.Text>().text = Time.timeScale.ToString("F1");
         massValueField.GetComponent<UnityEngine.UI.Text>().text = GetComponent<Rigidbody>().mass.ToString();
         heightValueField.GetComponent<UnityEngine.UI.Text>().text = transform.position.y.ToString();
-        potentialValueField.GetComponent<UnityEngine.UI.Text>().text = PE().ToString();
+        potentialValueField.GetComponent<UnityEngine.UI.Text>().text = pe.ToString();
+        kineticValueField.GetComponent<UnityEngine.UI.Text>().text = ke.ToString();
+        totalValueField.GetComponent<UnityEngine.UI.Text>().text = (pe+ke).ToString();
     }
 
 }
